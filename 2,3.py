fdbe07fc1e49395cfa04cc0fb6766cd511d68001
RFIXATE <s.cancinop@duocuc.cl>
2023-05-30 15:48:29 -0400

    2,31

--- 2,3.py
+++ 2,3.py
@@ -9,7 +9,7 @@ lectura = requests.get("https://dummyjson.com/quotes")
 if lectura.status_code == 200:
     data = lectura.json()
     cita = data['quotes']
-    valores = [[quote['author'], quote['quote']] for quote in cita]
+    valores = [[quote['autor'], quote['cita']] for quote in cita]
 
     with open("endpoint.csv", mode='w', newline='') as file:
         escritor = csv.writer(file, delimiter='\t')
